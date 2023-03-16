10c96d7959143777a2deb8a8fb75b97f07ac0622
cashiell <cashiell.sekori@gmail.com>
2023-03-16 00:43:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is apiece of art,\n");
+	return (0);
+}
+
