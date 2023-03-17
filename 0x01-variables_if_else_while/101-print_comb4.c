0fde51bc6344566a213144198d38c902a2e365b2
cashiell <cashiell.sekori@gmail.com>
2023-03-17 06:18:05 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = 0;
+	while (a <= 7)
+	{
+		b = a + 1;
+		while (c <= 9)
+		{
+			putchar(a + 48);
+			putchar(b + 48);
+			putchar(c + 48);
+			if (a < 7 || b < 8 || c < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			c++;
+		}
+		b++;
+	}
+	a++;
+}
+putchar('\n');
+return (0);
+}
