570442e320b73d21092356ad8ef4f154debe9af5
cashiell <cashiell.sekori@gmail.com>
2023-03-20 16:22:59 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
 void print_alphabet(void)
 {
 
-	char c;
+	int i;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 97; i <= 122; i++)
 	{
-		_putchar(c);
+		putchar(i);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
