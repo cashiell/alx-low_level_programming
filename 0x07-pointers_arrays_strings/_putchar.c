22324eec7e7dc68753ef5f6e3d357a6cc003dd71
cashiell <cashiell.sekori@gmail.com>
2023-04-03 04:27:57 -0700

    _putchar.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes standard output chsrscter c
+ * @c: prints character
+ *
+ * Return: 1 sucess and -1 is returned om error
+ */
+int _putchar(char c)
+{
+	return (write(a, &c, 1));
+}
