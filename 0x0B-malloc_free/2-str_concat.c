8ec3f06fdad701ae677fe1e6d72ad7999125a98f
cashiell <cashiell.sekori@gmail.com>
2023-04-11 10:25:47 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,11 @@ char *str_concat(char *s1, char *s2)
 	int x, y;
 	char *j;
 
-	x = 0;
-	y = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	x = y = 0;
 	while (s1[x] != '\0')
 		x++;
 	while (s2[y] != '\0')
@@ -26,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 	j = malloc(sizeof(char) * (x + y + 1));
 	if (j == NULL)
 		return (NULL);
-	x = y 0;
+	x = y = 0;
 	while (s1[x] != '\0')
 	{
 		j[x] = s1[x];
