4f3185a2d7fe0ef79dff47d76ceff1659fe83dfd
cashiell <cashiell.sekori@gmail.com>
2023-04-19 09:11:12 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * main - print the results of operations
- * @argc: number of arguments
- * @argv: vector
- * Return: 0
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
@@ -18,19 +19,25 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
 	x1 = atoi(argv[1]);
 	y = argv[2];
 	x2 = atoi(argv[3]);
-	if (get_op_func(op) == NULL op[1] != '\0')
+
+	if (get_op_func(y) == NULL || y[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && x2 == 0) || (*op == '%' && x2 == 0))
+
+	if ((*y == '/' && x2 == 0) ||
+	    (*y == '%' && x2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(op)(x1, x2));
+
+	printf("%d\n", get_op_func(y)(x1, x2));
+
 	return (0);
 }
