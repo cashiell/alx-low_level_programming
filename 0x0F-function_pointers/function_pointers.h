0533279cdb38c0f190728e8e7d920bb4e350b36f
cashiell <cashiell.sekori@gmail.com>
2023-04-18 23:46:16 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifdef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
