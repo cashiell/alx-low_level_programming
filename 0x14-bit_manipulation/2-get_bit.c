87d7d78f2ccdc775bab361d9ffba284b801831fe
cashiell <cashiell.sekori@gmail.com>
2023-05-05 02:53:52 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - return value of a bit at index
+ * @n: variable to search
+ * @index: bit index
+ *
+ * Return: bit value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int x;
+
+	if (index > 63)
+	       return (-1);
+	x = (n >> index) & 1;
+	return (x);
+}	
