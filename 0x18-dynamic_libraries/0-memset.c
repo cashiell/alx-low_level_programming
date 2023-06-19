64680603d775d6002c64a88f56000c4fc8e31e88
cashiell <cashiell.sekori@gmail.com>
2023-06-19 10:02:53 -0700

    task_0, dynamic library libdynamic.so

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - writes a function that fills memory with a constant byte.
+ * @n: The function fills the first n bytes of the memory area
+ * @s: pointed to by s
+ * @b: with the constant byte b
+ *
+ * Return: Apointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x = 0;
+
+	for (; n > 0; x++)
+	{
+		s[x] = b;
+		n--;
+	}
+	return (s);
+}
